0893b6c5c9ecba25fcfc59ac0dee82ae096b8bad
devjihad <mdhajihad609@gmail.com>
2026-02-24 15:15:05 +0600

    celcius calculator done

---
+++ index.js
@@ -0,0 +1,11 @@
+// celsius to fahrenheit
+let fahrenheit= 0
+const convert =(celsius)=>{
+    let result = (celsius*(9/5)+32)
+    fahrenheit = result
+}
+convert(104)
+let sum = Math.floor(fahrenheit)
+// let num = Math.floor(Math.random()*100)
+// console.log(num)
+console.log(sum,'F')
