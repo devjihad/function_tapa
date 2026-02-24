b4315042a2e6d1ba7e57f483c45c57d4da33bb4d
devjihad <mdhajihad609@gmail.com>
2026-02-24 16:24:51 +0600

    'done'

--- index.js
+++ index.js
@@ -22,3 +22,58 @@ function findMaximum(num1, num2 ){
 }
 
 findMaximum( 50,40)
+
+// factorial number
+
+const factorial = (num) =>{
+    if(num < 0 ){
+        console.log('please give a valid number')
+    }
+    else if( num ==0 || num == 1){
+        return 1 
+    }
+    else{
+        return num * factorial(num - 1 )
+    }
+}
+let main = factorial (6)
+console.log(main)
+
+// // let count = 0 
+
+// function counte(data){
+//     for (let i = 0 ; i<=data.length-1 ; i++){
+//         let demo = data[i]
+        
+//         if(demo === 'a' || 'e' || 'i'){
+//             count ++
+//         }
+//         else{
+//             console.log(demo)
+//         }
+            
+//     }
+// }
+
+// counte('dataddd')
+// console.log(count)
+// let count =0
+// const data =(strin)=>{
+//     let matche = strin.match( 'e' )
+//     if(matche){
+//         count ++
+//     }
+//     return matche 
+// }
+// let done = data('stringe')
+// console.log(done)
+// console.log(count)
+
+const callb = (done) =>{
+      function sum (dome) {
+        console.log(dome)
+      }
+      sum(done)
+}
+
+callb('sldkfj')
